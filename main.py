5d8874663d24e0df6bbf94fc3fc780bdde7d82c3
be_m1ya <52522955+be-m1ya@users.noreply.github.com>
2020-01-21 05:14:06 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import cv2
 import numpy as np
 
+#画像により調整
 thresh = 85
 max_val =255
-
 thresholdType = cv2.THRESH_BINARY
 
+#floatで画像をロード
 img = cv2.imread("img.bmp",0).astype( 'float32' ) 
 median = cv2.medianBlur(img,5)
 
-star =img - median
-cv2.imwrite("star.bmp",star)
+star = img - median
 ret,star = cv2.threshold(star,thresh,max_val,thresholdType)
-cv2.imwrite("star_binari.bmp",star)
+cv2.imwrite("star.bmp",star)
